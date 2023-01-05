1401fa5b8ab02345c7b07c5a4baff037bfe8fd1e
Vellyus <noisyboy026@gmail.com>
2023-01-05 18:41:08 +0100

    added HEAD and BODY

--- src/HangmanDrawing.js
+++ src/HangmanDrawing.js
@@ -1,6 +1,31 @@
+const HEAD = (
+  <div style={ {
+    width: "50px",
+    height: "50px",
+    borderRadius: "50%",
+    border: "10px solid black",
+    position: "absolute",
+    top: "50px",
+    right: "-30px"
+  } }></div>
+)
+
+const BODY = (
+  <div style={ {
+    width: "10px",
+    height: "100px",
+    background: "black",
+    position: "absolute",
+    top: "120px",
+    right: 0
+  } }></div>
+)
+
 export function HangmanDrawing() {
   return (
     <div style={ { position: "relative" } }>
+      { HEAD }
+      { BODY }
       <div style={ {
         height: "50px",
         width: "10px",
