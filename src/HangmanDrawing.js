68ee360e41cdd7d9f3e5ea451aa85d61134f1769
Vellyus <noisyboy026@gmail.com>
2023-01-07 21:50:46 +0100

    added arms and legs

--- src/HangmanDrawing.js
+++ src/HangmanDrawing.js
@@ -21,11 +21,70 @@ const BODY = (
   } }></div>
 )
 
+const RIGHT_ARM = (
+  <div style={ {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "150px",
+    right: "-100px",
+    rotate: "-30deg",
+    transformOrigin: "left bottom"
+  } }></div>
+)
+
+const LEFT_ARM = (
+  <div style={ {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "150px",
+    right: "10px",
+    rotate: "30deg",
+    transformOrigin: "right bottom"
+  } }></div>
+)
+
+const RIGHT_LEG = (
+  <div style={ {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "210px",
+    right: "-90px",
+    rotate: "60deg",
+    transformOrigin: "left bottom"
+  } }></div>
+)
+
+const LEFT_LEG = (
+  <div style={ {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    position: "absolute",
+    top: "210px",
+    right: 0,
+    rotate: "-60deg",
+    transformOrigin: "right bottom"
+  } }></div>
+)
+
+
+
+
 export function HangmanDrawing() {
   return (
     <div style={ { position: "relative" } }>
       { HEAD }
       { BODY }
+      { RIGHT_ARM }
+      { LEFT_ARM }
+      { RIGHT_LEG }
+      { LEFT_LEG }
       <div style={ {
         height: "50px",
         width: "10px",
