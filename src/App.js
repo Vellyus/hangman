2f4c9337a6c406b9f44b8033aeefa81791dc15f6
Vellyus <noisyboy026@gmail.com>
2023-01-15 12:11:17 +0100

    showing letters on keypress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import words from "./wordList.json"
 import { HangmanDrawing } from "./HangmanDrawing"
 import { HangmanWord } from "./HangmanWord"
@@ -14,7 +14,27 @@ function App() {
 
   const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
 
-  console.log(wordToGuess)
+  const addGuessedLetter = useCallback(letter => {
+    if (guessedLetters.includes(letter)) return
+    setGuessedLetters(currentLetters => [...currentLetters, letter])
+  }, [guessedLetters])
+
+  useEffect(() => {
+    const handler = e => {
+      const key = e.key
+
+      if (!key.match(/^[a-z]$/)) return
+
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetters])
 
   return (
     <div
@@ -29,7 +49,7 @@ function App() {
     >
       <div style={ { fontSize: "2rem", textAlign: "center" } }>Lose Win</div>
       <HangmanDrawing numberOfGuesses={ incorrectLetters.length } />
-      <HangmanWord />
+      <HangmanWord guessedLetters={ guessedLetters } wordToGuess={ wordToGuess } />
       <div style={ { alignSelf: "stretch" } }>
         <Keyboard />
       </div>
